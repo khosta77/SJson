1e7e521f9890087fc6108156551f230323f6a70c
khosta77 <puwerfulpants@gmail.com>
2022-07-18 19:15:54 +0300

    что то выходит с кавычками

--- main.cpp
+++ main.cpp
@@ -5,45 +5,62 @@
 
 using namespace std;
 
-void get_num(std::vector<double> &array, const char *str, const size_t i) {
-    static bool call = false;
-    if
-    if ((str[i] >= 48) && (str[i] <= 57)) {
-        if (str[i + 1] != '\0') {
-            cout << str[i] << " " << int(str[i]) << endl;
-        }
-        arr.push_back(static_cast<int>(int(str[i])) - '0');
-    }
-}
+//void get_num(std::vector<double> &array, const char *str, const size_t i) {
+//    static bool call = false;
+//    if
+//    if ((str[i] >= 48) && (str[i] <= 57)) {
+//        if (str[i + 1] != '\0') {
+//            cout << str[i] << " " << int(str[i]) << endl;
+//        }
+//        arr.push_back(static_cast<int>(int(str[i])) - '0');
+//    }
+//}
 
 int main(){
-    const char *str = "test1 0.98 Array 8";
+    const char *str = " \"test1\" : 0.98, \n \"Array\" : 8";
     vector<double> arr;
-    bool integer = false;  // Целое или не целое число
+    vector<string> name;
+    bool integer = true, plus = true;  // Целое или не целое число
     for (size_t i = 0; str[i] != '\0'; i++) {
-        int condition = int(str[i]);
-        switch (condition) {
-            case 46: {
-
-            }
-            case 95: {
-
-            }
-            default: {
-
+        if (str[i] == '\"') {
+            name.resize(name.size() + 1);
+            for (size_t j = i + 1;; j++) {
+                if (str[j] != '\"') {
+                    name[name.size() - 1] += str[j];
+                } else {
+                    i = j;
+                    break;
+                }
             }
         }
-        if ((str[i] >= 48) && (str[i] <= 57)) {
-            if (str[i + 1] == 46) {}
-            if (str[i + 1] != '\0') {
-                cout << str[i] << " " << int(str[i]) << endl;
-            }
-            arr.push_back(static_cast<int>(int(str[i])) - '0');
-        }
+//        if (((str[i] >= 48) && (str[i] <= 57)) || (str[i] == '+') || (str[i] == '-')) {
+//            if (str[i] == '-') {
+//                plus = false;
+//            }
+//            for (size_t j = i; str[j] != ',' && str[j] != '\0' && str[j] != ']'; j++) {
+//                if (integer) {
+//                    if ((str[i] >= 48) && (str[i] <= 57)) {
+//
+//                    }
+//                } else {
+//                    if ((str[i] >= 48) && (str[i] <= 57)) {
+//
+//                    }
+//                }
+//            }
+//        }
+//        int condition = int(str[i]);
+//        if ((str[i] >= 48) && (str[i] <= 57)) {
+//            if (str[i + 1] == 46) {}
+//            if (str[i + 1] != '\0') {
+//                cout << str[i] << " " << int(str[i]) << endl;
+//            }
+//            arr.push_back(static_cast<int>(int(str[i])) - '0');
+//        }
     }
 
-    for (size_t i = 0; i < arr.size(); i++) {
-        cout << arr[i] << " ";
+    for (size_t i = 0; i < name.size(); i++) {
+        cout << name[i] << " ";
     }
     cout << endl;
 
