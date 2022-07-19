f531e023450470d45a16aba526f0ab4ecf723910
khosta77 <puwerfulpants@gmail.com>
2022-07-19 18:54:56 +0300

    Написаны функции которые корректно считывают строку, число, массив

--- main.cpp
+++ main.cpp
@@ -5,64 +5,103 @@
 
 using namespace std;
 
-//void get_num(std::vector<double> &array, const char *str, const size_t i) {
-//    static bool call = false;
-//    if
-//    if ((str[i] >= 48) && (str[i] <= 57)) {
-//        if (str[i + 1] != '\0') {
-//            cout << str[i] << " " << int(str[i]) << endl;
-//        }
-//        arr.push_back(static_cast<int>(int(str[i])) - '0');
-//    }
-//}
+void read_string(vector<string> &name, const char *str, size_t &i) {
+    name.resize(name.size() + 1);
+    for (size_t j = i + 1;; j++) {
+        if (str[j] != '\"') {
+            name[name.size() - 1] += str[j];
+        } else {
+            i = j;
+            break;
+        }
+    }
+}
 
-int main(){
-    const char *str = " \"test1\" : 0.98, \n \"Array\" : 8";
-    vector<double> arr;
-    vector<string> name;
-    bool integer = true, plus = true;  // Целое или не целое число
-    for (size_t i = 0; str[i] != '\0'; i++) {
-        if (str[i] == '\"') {
-            name.resize(name.size() + 1);
-            for (size_t j = i + 1;; j++) {
-                if (str[j] != '\"') {
-                    name[name.size() - 1] += str[j];
+double read_num(const char *str, size_t &i) {
+    double num;  // Число
+    bool plus = false;  // Положительное число или отрицательное
+    if (str[i] == '-') {
+        plus = true;
+    }
+    bool integer = true;  // Целое или не целое число
+    bool first = true;  // Первая цифра
+    int trigger = 1;  // Счетчик, если число не целое
+    for (size_t j = i;; j++) {
+        if (str[j] == ',' || str[j] == '}' || str[j] == ' ' || str[j] == '\n') {
+            if (plus) {
+                num = num * (-1);
+            }
+            i = j;
+            break;
+        }
+        if ((str[j] >= 48) && (str[j] <= 57)) {
+            if (first) {
+                num += (static_cast<int>(int(str[j])) - '0');
+                first = false;
+            } else {
+                if (integer) {
+                    num *= 10;
+                    num += (static_cast<int>(int(str[j])) - '0');
                 } else {
-                    i = j;
-                    break;
+                    double buf = double((static_cast<int>(int(str[j])) - '0'));
+                    for (size_t k = 0; k < trigger; k++) {
+                        buf /= 10;
+                    }
+                    num += buf;
+                    trigger++;
                 }
             }
+        } else if (str[j] == '.') {
+            integer = false;
         }
-//        if (((str[i] >= 48) && (str[i] <= 57)) || (str[i] == '+') || (str[i] == '-')) {
-//            if (str[i] == '-') {
-//                plus = false;
-//            }
-//            for (size_t j = i; str[j] != ',' && str[j] != '\0' && str[j] != ']'; j++) {
-//                if (integer) {
-//                    if ((str[i] >= 48) && (str[i] <= 57)) {
-//
-//                    }
-//                } else {
-//                    if ((str[i] >= 48) && (str[i] <= 57)) {
-//
-//                    }
-//                }
-//            }
-//        }
-//        int condition = int(str[i]);
-//        if ((str[i] >= 48) && (str[i] <= 57)) {
-//            if (str[i + 1] == 46) {}
-//            if (str[i + 1] != '\0') {
-//                cout << str[i] << " " << int(str[i]) << endl;
-//            }
-//            arr.push_back(static_cast<int>(int(str[i])) - '0');
-//        }
+    }
+    return num;
+}
+
+void read_array(vector<double> &arr, const char *str, size_t &i) {
+    for (size_t j = i + 1;; j++) {
+        if (str[j] == ']') {
+            i = j;
+            break;
+        }
+        if (str[j] == ' ' || str[j] == '\n') {
+            continue;
+        }
+        arr.push_back(read_num( str, j));
+    }
+}
+
+int main(){
+    const char *str = "\"test1\" : [\n"
+                      "0.98, -1.257, -4, -15\n"
+                      "], \n "
+                      "\"Array\" : 8 ";
+    vector<double> arr;
+    vector<double> num;
+    vector<string> name;
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\"') {
+            read_string(name, str, i);
+        } else if (str[i] == '[') {
+            size_t m = i;
+            read_array(arr, str, i);
+            cout << m << " " << i << endl;
+        } else if (((str[i] >= 48) && (str[i] <= 57)) || (str[i] == '-')) {
+            num.push_back(read_num( str, i));
+        }
     }
 
     for (size_t i = 0; i < name.size(); i++) {
         cout << name[i] << " ";
     }
     cout << endl;
-
+    for (size_t i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (size_t i = 0; i < num.size(); i++) {
+        cout << num[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
