84de1621e9adea75454f60abf67e32d994f7f287
khosta77 <puwerfulpants@gmail.com>
2022-07-20 21:56:17 +0300

    Написан парсер, осталось только добавить комментариев

--- SJson.h
+++ SJson.h
@@ -8,24 +8,12 @@
 #include <iostream>
 #include <fstream>
 
-/*
- * Ну а если серьезно:
-1) Сделайте парсер, способный прочитать число.
-2) Сделайте парсер, способный прочитать строку в кавычках.
-3) Сделайте парсер, который будет делать пункты один и два, пока не напорется на закрывающую квадратную скобку.
- (Чтение json-массива).
-4) Добавьте в пункт три, выполнение пункта три - натолкнувшись на открывающую скобку, парсер вызывает себя рекурсивно.
-Вот уже будет какое-то примерное понимание как эта хреновина работает.
- * */
-
 class Json {
 private:
     typedef std::map<std::string, std::any> DataFrame;
+    DataFrame dataframe;
     inline static DataFrame data;
     inline static std::string last_name;
-
-    bool Array_status = false;
-    bool Object_status = false;
 
     static void read_string(std::string &word, const char *str, size_t &i) {
         word.clear();
@@ -93,7 +81,7 @@ private:
         }
     }
 
-    bool readfile(std::string& s, const char* filename){
+    static bool readfile(std::string& s, const char* filename){
         std::ifstream fp(filename);
         if(! fp.is_open())
             return false;
@@ -104,81 +92,84 @@ private:
         return (s.length() > 0);
     }
 
-
-    static DataFrame init_data() {
-        DataFrame d;
-        return d;
-    }
-public:
-    // Конструктор из строки, содержащей Json-данные.
-    Json() {}
-    Json(const std::string& s);
-
-    // Метод возвращает true, если данный экземпляр содержит в себе JSON-массив. Иначе false.
-    bool is_array() const;
-    // Метод возвращает true, если данный экземпляр содержит в себе JSON-объект. Иначе false.
-    bool is_object() const;
-
-    // Метод возвращает значение по ключу key, если экземпляр является JSON-объектом.
-    // Значение может иметь один из следующих типов: Json, std::string, double, bool или быть пустым.
-    // Если экземпляр является JSON-массивом, генерируется исключение.
-    std::any& operator[](const std::string& key);
-
-    // Метод возвращает значение по индексу index, если экземпляр является JSON-массивом.
-    // Значение может иметь один из следующих типов: Json, std::string, double, bool или быть пустым.
-    // Если экземпляр является JSON-объектом, генерируется исключение.
-    std::any& operator[](int index);
-
-    // Метод возвращает объект класса Json из строки, содержащей Json-данные.
-    static void parse(const std::string& s) {
+    static DataFrame parser(const std::string &s, size_t start) {
         bool map_status = false;
-        for (size_t i = 0; s[i] != '\0'; i++) {
+        DataFrame buf_data;
+        std::string buf;
+        for (size_t i = start; s[i] != '}'; i++) {
             if (s[i] == ':') {
                 map_status = true;
             } else if (s[i] == '\"') {
-                std::string buf = last_name;
+                buf = last_name;
                 read_string(last_name, s.c_str(), i);
                 if (map_status) {
-                    data[buf] = last_name;
-                    last_name.clear();
+                    buf_data[buf] = last_name;
                     buf.clear();
+                    last_name.clear();
                     map_status = false;
                 }
-            } else if (s[i] == '[') {
+            } else if ((s[i] == '[') && map_status) {
                 std::vector<double> arr;
                 read_array(arr, s.c_str(), i);
-                if (map_status) {
-                    data[last_name] = arr;
-                    arr.clear();
-                    last_name.clear();
-                    map_status = false;
-                }
-            } else if (((s[i] >= 48) && (s[i] <= 57)) || (s[i] == '-')) {
-                if (map_status) {
-                    data[last_name] = read_num(s.c_str(), i);
-                    last_name.clear();
-                    map_status = false;
-                }
+                buf_data[last_name] = arr;
+                arr.clear();
+                map_status = false;
+                last_name.clear();
+            } else if ((((s[i] >= 48) && (s[i] <= 57)) || (s[i] == '-')) && map_status) {
+                buf_data[last_name] = read_num(s.c_str(), i);
+                map_status = false;
+                last_name.clear();
+            } else if (((s[i] == 't') || (s[i] == 'f')) && map_status) {
+                buf_data[last_name] = (s[i] == 't') ? true : false;
+                map_status = false;
+                last_name.clear();
+            } else if ((s[i] == '{') && map_status) {
+                buf = last_name;
+                Json buf_obj(parser(s, (1 + i)));
+                buf_data[buf] = buf_obj;
+                buf.clear();
+                last_name.clear();
+                map_status = false;
             }
         }
+        return buf_data;
+    }
 
-        for (auto &[key, val] : data) {
-            if (val.type() == typeid(std::vector<double>)) {
-                std::cout << std::any_cast<std::string>(key) << " ";
-                std::vector<double> vec = std::any_cast<std::vector<double>>(val);
-                for (size_t i = 0; i < vec.size(); i++) {
-                    std::cout << vec[i] << " ";
-                }
-                std::cout << std::endl;
-            } else if (val.type() == typeid(std::string))
-                std::cout << std::any_cast<std::string>(key) << " " << std::any_cast<std::string>(val) << "\n";
-            else if (val.type() == typeid(double))
-                std::cout << std::any_cast<std::string>(key) << " " << std::any_cast<double>(val) << "\n";
-        }
+    Json(std::map<std::string, std::any> friend_data) {
+        dataframe = friend_data;
+    }
+public:
+    // Конструктор из строки, содержащей Json-данные.
+    Json() = default;
+
+    Json(const std::string& s) {
+        data = parser(s, 0);
+        this->dataframe = data;
+        data.clear();
+    }
+
+    // Метод возращает объект any
+    std::any operator[](const std::string& key) {
+        return dataframe[key];
+    }
+
+    // Метод возвращает объект класса Json из строки, содержащей Json-данные.
+    static Json parse(const std::string& s) {
+        return Json(parser(s, 0));
     }
 
     // Метод возвращает объекта класса Json из файла, содержащего Json-данные в текстовом формате.
-    static Json parseFile(const std::string& path_to_file);
-} object;
+    static Json parseFile(const std::string& path_to_file) {
+        std::string str;
+        if(readfile(str, path_to_file.c_str())) {
+            return Json(parse(str));
+        }
+        return Json();
+    }
+
+    ~Json() {
+        dataframe.clear();
+    }
+};
 
 #endif //SJSON_SJSON_H
